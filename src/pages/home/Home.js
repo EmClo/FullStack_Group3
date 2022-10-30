f8b8c5d64d9eedc259da4493f8d7b69c72b4ac81
Zainab Ahmed <zainy_989@hotmail.com>
2022-10-30 19:17:59 +0000

    responsiveness and layout changes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import GoogleMap from "../../components/GoogleMap";
 import styles from "./home.module.css";
+import logo from "../../components/navbar/images/logo.png";
 
 function Home() {
   return (
     <div>
+      <a>
+        <img src={logo} alt="Be Aware Everywhere logo" />
+      </a>
       <h1>Welcome</h1>
       <GoogleMap />
       {/* <div id="locationSearch">
--- src/pages/report/Report.js
+++ src/pages/report/Report.js
@@ -1,14 +1,55 @@
 import React from "react";
-import ReportCrime from "../../components/reportcrime";
 
+function ReportCrime() {
+  return (
+    <div class="mapbackground">
+      <h1>
+        {" "}
+        Please let us know if there is anything you would like to report about
+        crime in your area or any other queries
+      </h1>
+      <form>
+        <ul class="inputboxes">
+          <li>
+            <input
+              className="inputtextarea"
+              type="text"
+              name="user_name"
+              placeholder="First Name"
+            />
+          </li>
+          <li>
+            <input
+              className="inputtextarea"
+              type="text"
+              name="user_last_name"
+              placeholder="Last Name"
+            />
+          </li>
+          <li>
+            <input
+              className="inputtextarea"
+              type="email"
+              name="user_email"
+              placeholder="Your Email"
+            />
+          </li>
+        </ul>
+        <div class="inputboxesmessage">
+          <textarea
+            className="inputtextarea"
+            name="user_message"
+            placeholder="Your Message..."
+          />
+        </div>
+        <div>
+          <button class="clickbutton" type="submit">
+            Send Message
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
 
-function Report() {
-    return (
-      <div>
-        <h1>Contact Us</h1>
-        <ReportCrime />
-      </div>
-    );
-  }
-
-export default Report;
+export default ReportCrime;
