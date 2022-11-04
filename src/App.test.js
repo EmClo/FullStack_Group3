7927ac60c4c6b8416cd3d56bb79316a86ad306b0
Tanjin Huda <tanjin49@hotmail.it>
2022-11-04 02:18:41 +0000

    testing boilerplate/ structure added

---
+++ src/App.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen} from '@testing-library/react';
+import NavBar from "./components/navbar/NavBar";
+
+// setupTests.js
+
+// GooglePlaces.test.js (search bar test)
+
+// GoogleMap.test.jas (map rendering with crime info marker test)
+
+
+// NavBar.test.js 
+
+// testing the about link 
+test('render about link', () => {
+  render(<NavBar/>)
+  expect(screen.getByText(/about/)).toBeInTheDocument();
+})
+
+// testing the data link 
+// testing the home link 
+// testing the report link 
+// testing the safety link 
+
+// ReportCrime.test.js (emailjs component test)
